c239d2b1ccf9229df53f54b4c9d2964fe2f64712
Luís Resende <luisfrvilela@gmail.com>
2015-05-03 19:36:57 -0300

    Create CityTest

--- app/src/androidTest/java/com/modesteam/pardal/models/CityTest.java
+++ app/src/androidTest/java/com/modesteam/pardal/models/CityTest.java
@@ -1,22 +1,93 @@
 package com.modesteam.pardal.models;
 
+import com.modesteam.pardal.Pardal;
+
 import junit.framework.TestCase;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import helpers.Condition;
+import helpers.GenericPersistence;
+import helpers.Operator;
+import libraries.NotNullableException;
 import models.City;
+import models.State;
+import models.HighwayStretch;
 
 /**
  * Created by luisresende on 03/05/15.
  */
 public class CityTest extends TestCase {
+    //Metodos Iguais por classe(Mude o nome e os parametros)
+    City city1,city2;
+    State state;
+    public void setUp() throws SQLException, NotNullableException, ClassNotFoundException {
+        Pardal.getInstance().setDatabaseName("database_test.sqlite3.db");
+        state = new State("DF");
+        state.save();
+        city1 = new City("1","City1",1);
+        city2 = new City("2","City2",1);
+        city1.save();
+        city2.save();
+
+    }
+    public void tearDown() throws SQLException, ClassNotFoundException {
+        city1.delete();
+        city2.delete();
+        state.delete();
+    }
 
     public void testShouldGetFirstCityFromDatabase() throws SQLException, ClassNotFoundException {
-        assertEquals(City.get(1).getName(), City.first().getName());
+        assertEquals(city1.getName(), City.first().getName());
     }
 
     public void testShouldGetLastCityFromDatabase() throws SQLException, ClassNotFoundException {
-        assertEquals("XAMBIOA", City.last().getName());
+        assertEquals(city2.getName(), City.last().getName());
+    }
+
+    public void testShouldGetCityFromDatabase() throws SQLException, ClassNotFoundException {
+        assertEquals(city2.getName(), City.get(City.last().getId()).getName());
+    }
+
+    public void testShouldCountCityFromDatabase() throws SQLException, ClassNotFoundException {
+       assertEquals(2, City.count());
+    }
+
+    public void testShouldGetAllCityFromDatabase() throws SQLException, ClassNotFoundException {
+        assertEquals(2, City.getAll().size());
+    }
+    public void testShouldSaveCityFromDatabase() throws SQLException, ClassNotFoundException, NotNullableException {
+        assertEquals(2, City.getAll().size());
+        City city3 = new City("3","City3",1);
+        city3.save();
+        assertEquals(3, City.getAll().size());
+        city3.delete();
+    }
+    public void testShouldDeleteCityFromDatabase() throws SQLException, ClassNotFoundException, NotNullableException {
+        City city3 = new City("3","City3",1);
+        city3.save();
+        assertEquals(3, City.getAll().size());
+        city3.delete();
+        assertEquals(2, City.getAll().size());
+    }
+
+    //Metodos Diferentes por classe
+
+    public void testShouldGetWhereCityFromDatabase() throws SQLException, ClassNotFoundException, NotNullableException {
+        Condition condition = new Condition(new City(),"name", Operator.EQUAL,"City1");
+        assertEquals(city1.getName(), City.getWhere(condition).get(0).getName());
+    }
+
+//    public void testShouldGetStateOfCityFromDatabase() throws SQLException, ClassNotFoundException, NotNullableException {
+//        assertEquals(state.getName(), City.first().getState().getName());
+//    }
+
+    public void testShouldGetHighwayStretchesOfCityFromDatabase() throws SQLException, ClassNotFoundException, NotNullableException {
+        HighwayStretch highwayStretches = new HighwayStretch ("1",0,City.first().getId());
+        highwayStretches.save();
+        assertEquals(highwayStretches.getNumber(), City.first().getHighwayStretches().get(0).getNumber());
+        highwayStretches.delete();
     }
 
 
