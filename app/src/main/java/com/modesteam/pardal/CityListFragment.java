d843fa196cf2e80c89b16dadc584484b80066312
Luís Resende <luisfrvilela@gmail.com>
2015-05-26 21:30:51 -0300

    Created on CityListFragment search Algorithm

--- app/src/main/java/com/modesteam/pardal/CityListFragment.java
+++ app/src/main/java/com/modesteam/pardal/CityListFragment.java
@@ -3,12 +3,15 @@ package com.modesteam.pardal;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -47,7 +50,7 @@ public class CityListFragment extends Fragment implements AbsListView.OnItemClic
      * The Adapter which will be used to populate the ListView/GridView with
      * Views.
      */
-    private ListAdapter mAdapter;
+    private ArrayAdapter mAdapter;
 
     // TODO: Rename and change types of parameters
     public static CityListFragment newInstance(String param1, String param2) {
@@ -89,8 +92,26 @@ public class CityListFragment extends Fragment implements AbsListView.OnItemClic
         mListView = (AbsListView) view.findViewById(android.R.id.list);
         ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
 
+        EditText searchText = (EditText) view.findViewById(R.id.searchEditText);
         // Set OnItemClickListener so we can be notified on item clicks
         mListView.setOnItemClickListener(this);
+
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mAdapter.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         return view;
     }
@@ -116,7 +137,7 @@ public class CityListFragment extends Fragment implements AbsListView.OnItemClic
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if (null != mListener) {
-            City citySelected = CityContent.ITEMS.get(position);
+            City citySelected = (City)mAdapter.getItem(position);
             mListener.onFragmentInteraction(citySelected.getId(),CityDetailFragment.newInstance(citySelected));
         }
     }
