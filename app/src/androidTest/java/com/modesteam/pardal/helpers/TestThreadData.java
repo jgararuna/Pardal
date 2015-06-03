c225aac71a6155e073e627290f57be50f2cf1f5a
laercioSilva <laerciossjr@gmail.com>
2015-06-03 15:43:41 -0300

    Create more tests in TestThreadData

--- app/src/androidTest/java/com/modesteam/pardal/helpers/TestThreadData.java
+++ app/src/androidTest/java/com/modesteam/pardal/helpers/TestThreadData.java
@@ -1,17 +1,35 @@
 package com.modesteam.pardal.helpers;
 
 import com.modesteam.pardal.ThreadData;
+import com.modesteam.pardal.brand.BrandContent;
+import com.modesteam.pardal.city.CityContent;
+import com.modesteam.pardal.highwayStretch.HighwayStretchContent;
+import com.modesteam.pardal.model.ModelContent;
+import com.modesteam.pardal.state.StateContent;
+import com.modesteam.pardal.type.TypeContent;
 
 import junit.framework.TestCase;
+
+import java.sql.SQLException;
+
+import models.Brand;
+import models.City;
+import models.HighwayStretch;
+import models.Model;
+import models.State;
+import models.Type;
 
 /**
  * Created by luisresende on 03/06/15.
  */
 public class TestThreadData extends TestCase {
 
+    private ThreadData threadData;
+    private Thread thread;
+
     public void setUp(){
-        ThreadData threadData = new ThreadData();
-        Thread thread = new Thread(threadData);
+        threadData = new ThreadData();
+        thread = new Thread(threadData);
         thread.start();
     }
 
@@ -21,7 +39,27 @@ public class TestThreadData extends TestCase {
         assertNotNull(threadDataInstance);
     }
 
-    public void testShouldPreLoadTypeContent(){
-        
+    public void testShouldPreLoadTypeContent() throws ClassNotFoundException, SQLException{
+        assertEquals(Type.count(), TypeContent.ITEMS.size());
+    }
+
+    public void testShouldPreLoadBrandContent() throws ClassNotFoundException, SQLException{
+        assertEquals(Brand.count(), BrandContent.ITEMS.size());
+    }
+
+    public void testShouldPreLoadStateContent() throws ClassNotFoundException, SQLException{
+        assertEquals(State.count(), StateContent.ITEMS.size());
+    }
+
+    public void testShouldPreLoadHighwayStretchContent() throws ClassNotFoundException, SQLException{
+        assertEquals(HighwayStretch.count(), HighwayStretchContent.ITEMS.size());
+    }
+
+    public void testShouldPreLoadCityContent() throws ClassNotFoundException, SQLException{
+        assertEquals(City.count(), CityContent.ITEMS.size());
+    }
+
+    public void testShouldPreLoadModelContent() throws ClassNotFoundException, SQLException{
+        assertEquals(Model.count(), ModelContent.ITEMS.size());
     }
 }
