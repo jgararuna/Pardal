bde0758f57da96e3e78a4a00f2f33dcef209e060
jgararuna <jgararuna@hotmail.com>
2015-06-18 19:24:39 -0300

    commit

--- app/src/main/java/com/modesteam/pardal/CreditsFragment.java
+++ app/src/main/java/com/modesteam/pardal/CreditsFragment.java
@@ -20,7 +20,7 @@ public class CreditsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static findal String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM2 = "param2";
 
     
     // TODO: Rename and change types of parameters
